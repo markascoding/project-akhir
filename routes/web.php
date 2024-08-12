6009eaa177ccbbb6df90aa97b050371b5e8baa89
markascoding <ariprabowo.kanesa@gmail.com>
2024-08-12 10:12:39 +0700

    menambahkan form login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\TeacherController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TeacherController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::resources([
     'teacher' => TeacherController::class,
 ]);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
